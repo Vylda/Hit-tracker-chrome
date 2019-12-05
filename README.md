eb0e94a8fc0e21444714247b73c78587e92c2330
Vilem Lipold <vilem.lipold@firma.seznam.cz>
2019-12-05 16:31:46 +0100

    filtrování

--- README.md
+++ README.md
@@ -5,16 +5,40 @@
 
 ## Použití
 
-Hit tracker je nástroj určený pro snazší zobrazení požadavků na logování Seznam.cz (https://h.imedia.cz/hit). Po nainstalovávání do Chromu se objeví nový panel Hit tracker v Nástrojích pro vývojáře (otevřou se pomocí klávesy F12 nebo klávesovou zkratkou Ctrl + Shift + I).
+Hit tracker je nástroj určený pro snazší zobrazení požadavků na logování Seznam.cz (https://h.imedia.cz/hit). Po nainstalovávání do Firefoxu se objeví nový panel Hit tracker ve Vývojářských nástrojích (otevřou se pomocí klávesy F12).
 
-Po kliknutí na políčko ve sloupci Parameter value se data zobrazí jako objekt v konzoli Nástrojů pro vývojáře.
+Po otevření se začnou vypisovat všechna zachycená dat.
+
+Po kliknutí na políčko ve sloupci Parameter value se data zobrazí jako objekt v konzoli Vývojářských nástrojů.
+
+V pravém dolním rohu jsou dvě tlačítka:
+
+* <img style="vertical-align: middle;" src="https://raw.githubusercontent.com/Vylda/Hit-tracker/master/resources/filter.png"> **Filtr** -  otevře skryje okno filtru;
+* <img style="vertical-align: middle;" src="https://raw.githubusercontent.com/Vylda/Hit-tracker/master/resources/clear.png"> **Clear** - smaže všechna data z tabulky.
+
+### Filtr
+
+![](https://raw.githubusercontent.com/Vylda/Hit-tracker-chrome/master/resources/filterdata.png)
+
+V okně filtru můžete zadat do políčka text, při jehož  výskytu v datech se zobrazí odesílaná data.
+
+Zaškrtnutím políčka **RegExp** je text v políčku brán jako regulární výraz ([Psaní regulárních výrazů na MDN](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions#Writing_a_regular_expression_pattern), není třeba psát úvodní a koncové lomítko). Např. při zatrženém políčku *RegExp* a hledaném textu *"text":"[a-žA-Ž0-9]* +[a-žA-Ž0-9 ]+"*, budou zobrazena pouze data, kde je v atributu *text* víceslovná hodnota.
+
+Pokud zaškrtnete políčko **aA**, bude vyhledávání citlivé na velikost písmen.
+
+Nastavení fitru reflektují pouze nově poslaná data.
+
+## Firefox
 
 Pokud používáte prohlížeč Firefox, použijte [Hit tracker for Firefox](https://github.com/Vylda/Hit-tracker).
 
 ## Aktuální verze a download
 
+### 1.1
+[download](https://chrome.google.com/webstore/detail/hit-tracker-for-chrome/dlelffppicddeimbgkciimomlfbjeiom)
+#### Changelog
+* filtrování
 
 ### 1.0.1
-[download](https://chrome.google.com/webstore/detail/hit-tracker-for-chrome/dlelffppicddeimbgkciimomlfbjeiom)
 #### Changelog
 * první stabilní verze
--- source/js/panel.js
+++ source/js/panel.js
@@ -3,10 +3,52 @@ class Panel {
 		this._dom = {
 			log: document.querySelector("#log"),
 			clear: document.querySelector("#clear"),
+			filter: document.querySelector("#filter"),
+			filterValue: document.createElement("input"),
+			isRegexInput: document.createElement("input"),
+			isCaseSensitive: document.createElement("input"),
+			filterContainer: document.createElement("div")
 		};
 
+		this._filterOpen = false;
+
+		this._dom.filterContainer.id = "filter-container";
+
+		this._dom.filterValue.setAttribute("type", "text");
+		this._dom.filterValue.addEventListener("input", (e) => {
+			if (this._dom.isRegexInput.checked) {
+				this._testRegex(this._dom.filterValue.value);
+			} else {
+				this._dom.filterValue.removeAttribute("style");
+			}
+		});
+
+		let label = document.createElement("label");
+		label.title = "Jde o regulární výraz (RegExp)."
+		this._dom.isRegexInput.setAttribute("type", 'checkbox');
+		this._dom.isRegexInput.addEventListener("change", () => {
+			if (this._dom.isRegexInput.checked) {
+				this._testRegex(this._dom.filterValue.value);
+			} else {
+				this._dom.filterValue.removeAttribute("style");
+			}
+		})
+		label.appendChild(this._dom.isRegexInput);
+		label.appendChild(document.createTextNode("RegExp"));
+		this._dom.filterContainer.appendChild(label);
+
+		let labelCS = document.createElement("label");
+		labelCS.title = "Citlivé na velikost písmen."
+		this._dom.isCaseSensitive.setAttribute("type", 'checkbox');
+		labelCS.appendChild(this._dom.isCaseSensitive);
+		labelCS.appendChild(document.createTextNode("aA"));
+
+		this._dom.filterContainer.appendChild(this._dom.filterValue);
+		this._dom.filterContainer.appendChild(label);
+		this._dom.filterContainer.appendChild(labelCS);
 
 		this._dom.clear.addEventListener("click", this);
+		this._dom.filter.addEventListener("click", this);
 		this._dom.log.addEventListener("click", this);
 
 		chrome.devtools.network.onRequestFinished.addListener(e => { this._processItem(e); });
@@ -17,7 +59,48 @@ class Panel {
 	handleEvent(e) {
 		if (e.target == this._dom.clear) {
 			this._clear();
+		} else if (e.target.closest("button") == this._dom.filter) {
+			if (!this._filterOpen) {
+				document.body.appendChild(this._dom.filterContainer);
+				this._filterOpen = true;
+			} else {
+				this._dom.filterContainer.remove();
+				this._filterOpen = false;
+			}
 		}
+	}
+
+	_testRegex(re) {
+		this._dom.filterValue.removeAttribute("style");
+		if (!re) {
+			return true;
+		}
+		try {
+			let reo = new RegExp(re.trim(), this._dom.isCaseSensitive.checked ? "" : "i");
+			return reo;
+		} catch (e) {
+			this._dom.filterValue.style.color = "red";
+			return false;
+		}
+	}
+
+	_filterData(text) {
+		this._dom.filterValue.removeAttribute("style");
+		if (!text) { return true; }
+		let filterText = this._dom.filterValue.value.trim();
+		if (!filterText) { return true; }
+
+		if (this._dom.isRegexInput.checked) {
+			let re = this._testRegex(filterText);
+			if (re) {
+				return re.test(text);
+			}
+			return true;
+		}
+		if (this._dom.isCaseSensitive.checked) {
+			return text.includes(filterText);
+		}
+		return text.toLowerCase().includes(filterText.toLowerCase());
 	}
 
 	/**
@@ -35,7 +118,7 @@ class Panel {
 	_makeLine(row, d) {
 		try {
 			let dObj = JSON.parse(d)
-			if ("action" in dObj) {
+			if ("action" in dObj && this._filterData(d)) {
 				let action = "", data = [], keys = Object.keys(dObj).sort();
 
 				keys.forEach(key => {
